141814c21b90c3ef2efa923924622fb922ded6b1
Bharathan <2b05.2201395.bharathanbhaskaran@gmail.com>
2025-09-28 22:25:00 +0800

    Edited the main.py file due to Access-Control-Allow-Origins issue

--- backend/main.py
+++ backend/main.py
@@ -14,17 +14,21 @@ origins = [
     "https://hack-it-rx-front-end.vercel.app",
     "http://localhost:3000",
 ]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,        
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+@app.get("/")
+def read_root():
+    return {"status": "ok"}
+
 # In-memory storage for sessions
 SESSIONS = {}
-
 
 # ---------------------------
 # MODELS
